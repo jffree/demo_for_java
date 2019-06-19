0e39cc100878c4a33fb8653eb9e01449ad17a0f0
Liwei Wang <wlw9001@163.com>
2019-06-20 01:53:33 +0800

    add aio network model demo

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # demo_for_java
-Some demo code based on java.
+
+*Some demo code based on java.*
 
 ## network model
-
-*Reference:   [Scalable IO in Java](http://gee.cs.oswego.edu/dl/cpjslides/nio.pdf)*
 
 * [Bio Model (use Thread)](https://github.com/jffree/demo_for_java/tree/master/src/cn.wthinker/java_demo/network_model/bio_model)
 
@@ -14,3 +13,11 @@ Some demo code based on java.
 * [Nio Reactor Model With MultiHandler](https://github.com/jffree/demo_for_java/tree/master/src/cn.wthinker/java_demo/network_model/reactor_with_multiHandler)
 
 * [Nio Reactor Model With MultiReactor](https://github.com/jffree/demo_for_java/tree/master/src/cn.wthinker/java_demo/network_model/reactor_with_subReactor)
+
+
+* [Aio Model](https://github.com/jffree/demo_for_java/tree/master/src/cn.wthinker/java_demo/network_model/aio_model)
+
+* **Reference:**   
+  1. *[Scalable IO in Java](http://gee.cs.oswego.edu/dl/cpjslides/nio.pdf)*
+  2. *《Netty 权威指南（第二版）》*
+
---
+++ src/cn.wthinker/java_demo/network_model/aio_model/AioServer.java
@@ -0,0 +1,53 @@
+package java_demo.network_model.aio_model;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.util.concurrent.CountDownLatch;
+
+public class AioServer extends Thread{
+    private String host;
+    private int port;
+    private AsynchronousServerSocketChannel serverSocketChannel;
+    private CountDownLatch countDownLatch = new CountDownLatch(1);
+
+    public AioServer(String host, int port) throws IOException {
+        this.host = host;
+        this.port = port;
+        serverSocketChannel = AsynchronousServerSocketChannel.open();
+        serverSocketChannel.bind(new InetSocketAddress(host, port));
+        System.out.println("Server run on port: " + this.port);
+    }
+
+    public void cancel(){
+        countDownLatch.countDown();
+    }
+
+
+    public AsynchronousServerSocketChannel getServerSocketChannel(){
+        return serverSocketChannel;
+    }
+
+    @Override
+    public void run(){
+        serverSocketChannel.accept(this, new AcceptCompleteHandler());
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            serverSocketChannel.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            new AioServer("0.0.0.0", 6211).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
