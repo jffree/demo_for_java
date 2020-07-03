c2bcb37b3367e3931e589e29f18eae91f7e5782c
lwwang <wlw9001@163.com>
2020-07-03 10:18:13 +0800

    add protobuf testcase

--- src/main/java/com/jffree/java_demo/protocol/protobuf/TestPersonProbuf.java
+++ src/main/java/com/jffree/java_demo/protocol/protobuf/TestPersonProbuf.java
@@ -1,6 +1,5 @@
 package com.jffree.java_demo.protocol.protobuf;
 
-
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.jffree.java_demo.protocol.protobuf.PersonProbuf.Person;
 import com.jffree.java_demo.protocol.protobuf.PersonProbuf.Person.PhoneNumber;
@@ -8,33 +7,30 @@ import com.jffree.java_demo.protocol.protobuf.PersonProbuf.Person.PhoneNumber;
 import java.util.List;
 
 public class TestPersonProbuf {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         PersonProbuf.Person.Builder builder = PersonProbuf.Person.newBuilder();
         builder.setEmail("JohnDoe@email.com");
         builder.setId(1);
         builder.setName("John Doe");
-        builder.addPhone(PersonProbuf.Person.PhoneNumber.newBuilder().setNumber("1001").setType(PersonProbuf.Person.PhoneType.MOBILE));
-        builder.addPhone(PersonProbuf.Person.PhoneNumber.newBuilder().setNumber("1002").setType(PersonProbuf.Person.PhoneType.HOME));
+        builder.addPhone(PersonProbuf.Person.PhoneNumber.newBuilder().setNumber("1001")
+            .setType(PersonProbuf.Person.PhoneType.MOBILE));
+        builder.addPhone(PersonProbuf.Person.PhoneNumber.newBuilder().setNumber("1002")
+            .setType(PersonProbuf.Person.PhoneType.HOME));
 
         Person person = builder.build();
         byte[] buf = person.toByteArray();
 
-        try
-        {
+        try {
             Person person2 = PersonProbuf.Person.parseFrom(buf);
 
             System.out.println(person2.getName() + ", " + person2.getEmail());
 
             List<PhoneNumber> lstPhones = person2.getPhoneList();
 
-            for (PhoneNumber phoneNumber : lstPhones)
-            {
+            for (PhoneNumber phoneNumber : lstPhones) {
                 System.out.println(phoneNumber.getNumber());
             }
-        }
-        catch (InvalidProtocolBufferException e)
-        {
+        } catch (InvalidProtocolBufferException e) {
             e.printStackTrace();
         }
 
