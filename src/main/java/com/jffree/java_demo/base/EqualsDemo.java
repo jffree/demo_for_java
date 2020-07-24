71abbc9dab4bcd22d8f487f920bdb1534f134568
lwwang <wlw9001@163.com>
2020-07-24 10:23:50 +0800

    add random equal and compare demo

--- src/main/java/com/jffree/java_demo/base/EqualsDemo.java
+++ src/main/java/com/jffree/java_demo/base/EqualsDemo.java
@@ -5,6 +5,41 @@ import java.util.Objects;
 
 public class EqualsDemo {
 
+    public static  class Employee {
+
+        private String name;
+        private double salary;
+        private Date hireDay;
+
+        public Employee(String name, double salary, Date hireDay) {
+            this.name = name;
+            this.salary = salary;
+            this.hireDay = hireDay;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            // 如果为同一对象的不同引用,则相同
+            if (this == obj) {
+                return true;
+            }
+            // 如果传入的对象为空,则返回false
+            if (obj == null) {
+                return false;
+            }
+
+            // 如果两者属于不同的类型,不能相等
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+
+            // 类型相同, 比较内容是否相同
+            Employee other = (Employee) obj;
+
+            return Objects.equals(name, other.name) && salary == other.salary && Objects.equals(hireDay, other.hireDay);
+        }
+    }
+
     public static void main(String[] args) {
         Date current = new Date();
         Employee e1 = new Employee("xiaowang", 12, current);
@@ -19,37 +54,3 @@ public class EqualsDemo {
 
 }
 
-class Employee {
-
-    private String name;
-    private double salary;
-    private Date hireDay;
-
-    public Employee(String name, double salary, Date hireDay) {
-        this.name = name;
-        this.salary = salary;
-        this.hireDay = hireDay;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        // 如果为同一对象的不同引用,则相同
-        if (this == obj) {
-            return true;
-        }
-        // 如果传入的对象为空,则返回false
-        if (obj == null) {
-            return false;
-        }
-
-        // 如果两者属于不同的类型,不能相等
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-
-        // 类型相同, 比较内容是否相同
-        Employee other = (Employee) obj;
-
-        return Objects.equals(name, other.name) && salary == other.salary && Objects.equals(hireDay, other.hireDay);
-    }
-}
