413391687c972b15d6f062e528ec2c83bc791b8a
lwwang <wlw9001@163.com>
2020-07-29 16:32:37 +0800

    add java atomic test

---
+++ src/main/java/com/jffree/java_demo/concurrent/thread_test/ThreadYieldTest.java
@@ -0,0 +1,25 @@
+package com.jffree.java_demo.concurrent.thread_test;
+
+// YieldTest.java的源码
+class ThreadA extends Thread{
+    public ThreadA(String name){
+        super(name);
+    }
+    public synchronized void run(){
+        for(int i=0; i <10; i++){
+            System.out.printf("%s [%d]:%d\n", this.getName(), this.getPriority(), i);
+            // i整除4时，调用yield
+            if (i%4 == 0)
+                Thread.yield();
+        }
+    }
+}
+
+public class ThreadYieldTest{
+    public static void main(String[] args){
+        ThreadA t1 = new ThreadA("t1");
+        ThreadA t2 = new ThreadA("t2");
+        t1.start();
+        t2.start();
+    }
+}
